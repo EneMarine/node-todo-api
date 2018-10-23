932772c2a50ea8d73a9b9aff495c72024b98b6b2
Marine <marine.lemetayer.pro@gmail.com>
2018-10-23 12:17:09 +0200

    Changement du nom de variable de BDD

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const DbName = 'TodoApp'; // Database Name
-const Url = process.env.MONGODB_URI || `mongodb://localhost:27017/${DbName}`; // Connection URL
+const Url = process.env.MONGOLAB_URI || `mongodb://localhost:27017/${DbName}`; // Connection URL
 
 // mongoose.Promise = global.Promise; //Paramètre quel type de Promise utiliser
 mongoose.connect( Url );
